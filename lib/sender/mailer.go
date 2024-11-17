73e5c2c51db1fa87a07e1a90c828d78d27b5a56e
tomasizuel <tomasizuel@gmail.com>
2024-11-17 16:36:58 -0300

    mail sender

--- lib/sender/mailer.go
+++ lib/sender/mailer.go
@@ -1,7 +1,34 @@
 package mailer
 
-import "mailgo/modules/template"
+import (
+	"fmt"
+	gomail "gopkg.in/mail.v2"
+	"mailgo/modules/template"
+)
 
 func SendEmail(mail template.MailNotificationTemplate, recipient string) error {
+	// Create a new message
+	message := gomail.NewMessage()
+
+	// Set email headers
+	message.SetHeader("From", "tomasizuel@gmail.com")
+	message.SetHeader("To", recipient)
+	message.SetHeader("Subject", mail.Subject)
+
+	// Set email body
+	message.SetBody("text/html", mail.BodyHTML)
+	message.AddAlternative("text/plain", mail.BodyText)
+
+	// Set up the SMTP dialer
+	dialer := gomail.NewDialer("live.smtp.mailtrap.io", 587, "", "")
+
+	// Send the email
+	if err := dialer.DialAndSend(message); err != nil {
+		fmt.Println("Error:", err)
+		panic(err)
+	} else {
+		fmt.Println("Email sent successfully!")
+	}
+
 	return nil
 }
